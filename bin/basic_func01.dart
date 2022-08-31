b2410d8cce979358a51e2576d5e0f03d78f0a85e
timur <ttoshte@gmail.com>
2022-08-31 09:15:42 +0300

    tarc

--- bin/basic_func01.dart
+++ bin/basic_func01.dart
@@ -1,4 +1,14 @@
 // Create a function called func.
 // Create two arguments type int
 //  Return the summ two arguments
-void main() {}
+int func(int a,int r){
+    
+    return a+r;
+} 
+void main(){
+    print(func(6,6));    
+}
+
+
+
+
