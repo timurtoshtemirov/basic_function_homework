d12568c030d5a68019b01a6865c75ffef30eee02
timur <ttoshte@gmail.com>
2022-08-31 10:25:41 +0300

    daer

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -2,7 +2,7 @@
 //Create function arguments a and b of type int. 
 //Returns the remainder of a and b.
 void main() {
-print (func(5,3));    
+print (func(6,3));    
 }
 int func (a,b){
 return a%=b;
