3a0ef9b11c12d087b55bcd6b5089f7b205ae106f
timur <ttoshte@gmail.com>
2022-08-31 10:49:15 +0300

    tyui

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -1,4 +1,13 @@
 // Create a function called func.
 // Create a fucntion argument ‘number’ two digits numbers will be given.
 // return the sum of the number’s digits.
-void main() {}
+void main() {
+print(func(45));    
+}
+int func (a){
+int d;
+int b;
+d=a~/10;
+b=a%10;
+return b+d;    
+}
