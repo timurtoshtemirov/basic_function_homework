a6355becf6934f439d6c0354a98b9185573d1229
timur <ttoshte@gmail.com>
2022-08-31 10:37:29 +0300

    rew

--- bin/basic_func04.dart
+++ bin/basic_func04.dart
@@ -1,4 +1,9 @@
 // Create a function called func.
 // Create function arguments a and b of type int. 
 // Return the multiplication of a and b.
-void main() {}
+void main() {
+print(func(5,3));    
+}
+int func (a,b){
+return a*b;    
+}
